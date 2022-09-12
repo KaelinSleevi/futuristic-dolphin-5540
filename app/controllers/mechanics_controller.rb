f6a37d7224684cbcbdc313e1a77b8d34c70ec8b1
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-09-12 11:53:16 -0600

    feat: in progress user story 3

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -5,6 +5,23 @@ class MechanicsController < ApplicationController
 
     def show
         @mechanics = Mechanic.find(params[:id])
+
+        if params[:search].present?
+            @rides = Ride.search(params[:search])
+        end
+    end
+
+    def edit
+        @mechanics = Mechanic.find(params[:id])
+
+    end
+
+    def update
+        @mechanics = Mechanic.find(params[:id])
+
+        @mechanics.update(params[:ride_id])
+        @mechanics.save
+        redirect_to "/mechanics/#{@mechanics.id}"
     end
 
 end
